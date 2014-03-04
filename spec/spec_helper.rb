e1d16bb5b8c6efa02aff036823724ad1aeed3e4b
John Kamenik <jkamenik@gmail.com>
2014-03-03 22:43:22 -0500

    minor spec behavior changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,11 +19,12 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  config.fail_fast = true
+  #config.fail_fast = true
   config.include Features, type: :feature
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
   config.use_transactional_fixtures = false
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
 
 Capybara.javascript_driver = :webkit
