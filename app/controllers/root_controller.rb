250dcc809907e6e2d06f28f922090c8e406c5802
John Kamenik <jkamenik@gmail.com>
2014-03-03 23:29:27 -0500

    Fixing parsing of the uploaded file

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -8,7 +8,7 @@ class RootController < ApplicationController
   end
 
   def legacy_import_submit
-    imported_items = CustomersProduct.from_legacy params[:file]
+    imported_items = CustomersProduct.from_legacy_csv params[:file].tempfile.to_path.to_s
 
     revenue = imported_items.sum(&:total_price)
     redirect_to :legacy_import, :notice => "Gross Revenue #{number_to_currency revenue}"
