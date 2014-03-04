0a8d2026b08da8975156770ead8c276785a460af
John Kamenik <jkamenik@gmail.com>
2014-03-03 22:43:02 -0500

    Testing customer import behavior

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,14 @@
 class Customer < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+
+  def self.from_legacy(row)
+    cust = self.where(name: row['purchaser name']).first
+
+    unless cust
+      cust = self.new name: row['purchaser name']
+      cust.save
+    end
+
+    cust
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Customer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#from_legacy' do
+    let(:test_row) do
+      {
+        'purchaser name'   => 'Snake Plissken',
+        'item description' => '$10 off $20 of food',
+        'item price'       => '10.0',
+        'purchase count'   => '2',
+        'merchant address' => '987 Fake St',
+        'merchant name'    => "Bob's Pizza"
+      }
+    end
+
+    it 'tries to find a customer by name before creating a new one' do
+      cust = create :customer, name: test_row['purchaser name']
+
+      expect {
+        Customer.from_legacy test_row
+      }.to change(Customer,:count).by(0)
+    end
+
+    it 'saves a new customer if one does not exist' do
+      expect {
+        Customer.from_legacy test_row
+      }.to change(Customer,:count).by(1)
+    end
+
+    it 'pulls name from "purchaser name"' do
+      cust = Customer.from_legacy test_row
+      expect(cust.name).to eq test_row['purchaser name']
+    end
+  end
 end
